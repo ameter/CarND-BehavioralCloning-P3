9017aaa044a593bed22be24f7af33f36e5859843
Chris Ameter <cameter@gmail.com>
2017-12-02 09:44:31 -0700

    tuned to 7 epochs

--- model.py
+++ model.py
@@ -92,7 +92,7 @@ history = model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples) * 6,
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples) * 6,
-                    nb_epoch=2,
+                    nb_epoch=7,
                     verbose=2)
 
 model.save('model.h5')

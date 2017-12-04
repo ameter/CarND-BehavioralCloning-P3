fc0ad9245d5cf3caf24759082d306c60c4731be8
Chris Ameter <cameter@gmail.com>
2017-12-03 18:41:14 -0700

    adjusted dropout rates.

--- model.py
+++ model.py
@@ -77,22 +77,22 @@ model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 # Model based on nvidia model (5 convolutional layers, flatten, 4 (counting output) fully connected layers)
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(100))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Dense(50))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Dense(10))
-model.add(Dropout(0.8))
+model.add(Dropout(0.25))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
@@ -100,7 +100,7 @@ history = model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples) * 6,
                     validation_data=validation_generator,
                     nb_val_samples=len(validation_samples) * 6,
-                    nb_epoch=50,
+                    nb_epoch=10,
                     verbose=2)
 
 model.save('model.h5')
